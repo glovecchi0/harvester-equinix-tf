3ec4099b3c739b88bbec19c35d3604167f16158a
Giovanni Lo Vecchio <glovecchio@MacBook-Pro.homenet.telecomitalia.it>
2024-10-22 09:06:01 +0200

    Added Longhorn URL output

--- examples/custom-vm-network/outputs.tf
+++ examples/custom-vm-network/outputs.tf
@@ -2,6 +2,10 @@ output "harvester_url" {
   value = module.harvester_equinix.harvester_url
 }
 
+output "longhorn_url" {
+  value = "${module.harvester_equinix.harvester_url}dashboard/c/local/longhorn"
+}
+
 output "equinix_first_server_public_ip" {
   value = module.harvester_equinix.seed_ip
 }
